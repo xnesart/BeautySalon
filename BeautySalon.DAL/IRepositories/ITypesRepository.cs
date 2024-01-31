ffe254af3e6f61ee65d2d09654a11dba8d8aed53
ustinova <rykova.tanya.96@gmail.com>
2024-01-31 23:55:56 +0300

    Add CetServicesType

---
+++ BeautySalon.DAL/IRepositories/ITypesRepository.cs
@@ -0,0 +1,14 @@
+﻿using BeautySalon.DAL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeautySalon.DAL.IRepositories
+{
+    public interface ITypesRepository
+    {
+        public List<TypesDTO> GetServiceType();
+    }
+}
---
+++ BeautySalon.DAL/Repositories/ServicesRepository.cs
@@ -0,0 +1,18 @@
+﻿using BeautySalon.DAL.DTO;
+using BeautySalon.DAL.StoredProcedures;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace BeautySalon.DAL.Repositories
+{
+    public class ServicesRepository
+    {
+        
+    }
+}
