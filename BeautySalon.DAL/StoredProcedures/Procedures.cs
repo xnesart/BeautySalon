17173ec3907cfab6e035fde065a0f8d4c8c103cf
xnesart <xnesart@mail.ru>
2024-02-02 21:38:30 +0300

    repairing procedures

--- BeautySalon.DAL/StoredProcedures/Procedures.cs
+++ BeautySalon.DAL/StoredProcedures/Procedures.cs
@@ -4,7 +4,8 @@ public class Procedures
 {
     public const string GetAllOrdersForClientProcedure = "GetAllOrdersForClient";
     public const string GetAllEmployeesProcedure = "GetAllEmployees";
-    public const string GetServiceTupe = "GetServiceType";
+    public const string GetServiceType = "GetServiceType";
     public const string GetServicesWithPriceAndDuratonByTypeId = "GetServicesWithPriceAndDuratonByTypeId";
     public const string GetAllShiftsWithFreeIntervalsOnCurrentService = "GetAllShiftsWithFreeIntervalsOnCurrentService";
+    public const string GetAllRolesProcedure = "GetAllRolesProcedure";
 }
