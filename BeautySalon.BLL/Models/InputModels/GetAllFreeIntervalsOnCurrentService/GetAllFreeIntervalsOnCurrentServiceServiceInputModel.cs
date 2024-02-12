99d02884385eaca985b9ca9823d77722506cca1c
xnesart <xnesart@mail.ru>
2024-02-12 18:20:50 +0300

    added succesed

---
+++ BeautySalon.BLL/Models/InputModels/GetAllFreeIntervalsOnCurrentService/GetAllFreeIntervalsOnCurrentServiceServiceInputModel.cs
@@ -0,0 +1,9 @@
+namespace BeautySalon.BLL.Models;
+
+public class GetAllFreeIntervalsOnCurrentServiceServiceInputModel
+{
+    public int? Id { get; set; }
+
+    public string? Title { get; set; }
+
+}
---
+++ BeautySalon.BLL/Models/InputModels/GetAllFreeIntervalsOnCurrentService/GetAllFreeIntervalsOnCurrentServiceShiftInputModel.cs
@@ -0,0 +1,10 @@
+namespace BeautySalon.BLL.Models;
+
+public class GetAllFreeIntervalsOnCurrentServiceShiftInputModel
+{
+    public int? Id { get; set; }
+
+    public string? Title { get; set; }
+
+    public DateTime? StartTime { get; set; }
+}
