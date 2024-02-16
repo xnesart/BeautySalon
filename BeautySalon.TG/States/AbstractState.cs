2a516676c7e8bbf67a9d2ce21e0784ac6130e535
xnesart <xnesart@mail.ru>
2024-02-16 15:10:39 +0300

    start working on states

---
+++ BeautySalon.TG/States/AbstractState.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types;
+
+namespace BeautySalon.TG.States;
+
+public abstract class AbstractState
+{
+    public abstract void SendMessage(long chatId);
+    public abstract AbstractState ReceiveMessage(Update update);
+}
---
+++ BeautySalon.TG/States/StartState.cs
@@ -0,0 +1,16 @@
+﻿using Telegram.Bot.Types;
+
+namespace BeautySalon.TG.States;
+
+public class StartState:AbstractState
+{
+    public override void SendMessage(long chatId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override AbstractState ReceiveMessage(Update update)
+    {
+        throw new NotImplementedException();
+    }
+}
