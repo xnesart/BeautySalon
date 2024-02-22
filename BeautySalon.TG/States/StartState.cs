a8db10c1cc06525a28c84d5754f9af9db1632230
xnesart <xnesart@mail.ru>
2024-02-22 12:22:00 +0300

    upd

--- BeautySalon.TG/States/StartState.cs
+++ BeautySalon.TG/States/StartState.cs
@@ -18,10 +18,14 @@ public class StartState : AbstractState
 
     public override AbstractState ReceiveMessage(Update update)
     {
-        if (update.Message.Text == "/admin")
+        if (update.Message != null)
         {
-            return new AdminState();
+            if (update.Message.Text == "/admin")
+            {
+                return new AdminState();
+            } 
         }
+        
         //проверяем, что пришедшее сообщение является нажатием на кнопку и не равно null
         if (update.Type == UpdateType.CallbackQuery && UpdateType.CallbackQuery != null)
         {
