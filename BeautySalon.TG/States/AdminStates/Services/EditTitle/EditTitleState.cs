674b7485ea8eddd409b0ca3ea8521c83ee4c00d2
xnesart <xnesart@mail.ru>
2024-02-23 20:46:03 +0300

    Added new states folders

---
+++ BeautySalon.TG/States/AdminStates/Services/EditTitle/EditTitleState.cs
@@ -0,0 +1,17 @@
+﻿using BeautySalon.TG.States;
+using Telegram.Bot.Types;
+
+namespace BeuatySalon.TG.States.Services;
+
+public class EditTitleState: AbstractState
+{
+    public override void SendMessage(long chatId, Update update, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override AbstractState ReceiveMessage(Update update)
+    {
+        throw new NotImplementedException();
+    }
+}
