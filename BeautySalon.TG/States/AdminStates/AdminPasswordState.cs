cefcdfc4c2bbc9273fa74e1190e78192fa3ff617
xnesart <xnesart@mail.ru>
2024-02-24 16:35:06 +0300

    upd

--- BeautySalon.TG/States/AdminStates/AdminPasswordState.cs
+++ BeautySalon.TG/States/AdminStates/AdminPasswordState.cs
@@ -20,7 +20,7 @@ public class AdminPasswordState : AbstractState
     {
         UserClient userClient = new UserClient();
         string adminName = userClient.GetWorkerNameByPassword(update.Message.Text);
-        if (adminName != null)
+        if (adminName != "")
         {
             Password = update.Message.Text;
             userClient.ChangeChatIdAndUserNameByPassword(Password, (int)update.Message.Chat.Id, update.Message.Chat.Username);
@@ -28,7 +28,13 @@ public class AdminPasswordState : AbstractState
         }
         else
         {
-            return this;
+            if (update.Message != null)
+            {
+                SingletoneStorage.GetStorage().Client.SendTextMessageAsync(update.Message.Chat.Id,
+                    "Введённый пароль отсутствует в базе. Попробуйте другой пароль или вернитесь в меню клиента.");
+                return new AdminState(); 
+            }
         }
+        return new StartState();
     }
 }
